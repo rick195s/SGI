44d02cb0d9d7c7dae326586a61a33390fdb5fd95
rick195s <ricardosf195@gmail.com>
2021-12-07 19:18:31 +0000

    change preview v.0.1

--- website/js/product_editor.js
+++ website/js/product_editor.js
@@ -1,4 +1,4 @@
-var renderer, camara, cena, directLight;
+var renderer, camera, scene, directLight;
 var inter, clickedObject;
 var model = new Model();
 
@@ -7,64 +7,64 @@ renderizar();
 
 function init() {
     var raycaster = new THREE.Raycaster();
-    var rato = new THREE.Vector2();
+    var mouse = new THREE.Vector2();
     var width = document.getElementById("canvasDiv").offsetWidth;
 
-    //------------------------------------------------ CRIAR CENA ---------------------------------------------------
-    cena = new THREE.Scene();
-    cena.background = new THREE.Color(0xffffff);
+    //------------------------------------------------ CRIAR scene ---------------------------------------------------
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
 
-    var meuCanvas = document.getElementById("myCanvas");
-    renderer = create_render(meuCanvas);
+    var canvas = document.getElementById("myCanvas");
+    renderer = create_render(canvas);
     renderer.toneMapping = THREE.ReinhardToneMapping;
     renderer.toneMappingExposure = 1.8;
 
     //------------------------------------------------ CRIAR OBJETO PARA RENDER  ---------------------------------------------------
-    camara = create_perspective_camera(window.innerWidth / window.innerHeight);
-    cena.add(camara);
+    camera = create_perspective_camera(window.innerWidth / window.innerHeight);
+    scene.add(camera);
 
-    //------------------------------------------------ ADICIONAR CONTROLOS DE CENARIO ---------------------------------------------------
-    new THREE.OrbitControls(camara, renderer.domElement);
+    //------------------------------------------------ ADICIONAR CONTROLOS DE sceneRIO ---------------------------------------------------
+    new THREE.OrbitControls(camera, renderer.domElement);
 
-    update_window(renderer, camara, width, window.innerHeight);
+    update_window(renderer, camera, width, window.innerHeight);
 
-    load_gltf_to(cena, "3D Model/workBenchM.gltf", model);
-    add_light_to(cena);
+    load_gltf_to(scene, "3D Model/workBenchM.gltf", model);
+    add_light_to(scene);
 
     // Resize canvas when page is resized
     window.addEventListener(
         "resize",
         () => {
             width = document.getElementById("canvasDiv").offsetWidth;
-            update_window(renderer, camara, width, window.innerHeight);
+            update_window(renderer, camera, width, window.innerHeight);
         },
         false
     );
 
-    myCanvas.addEventListener("mousemove", (evento) => onHover(evento, rato, raycaster));
-    // invocar raycaster
-    myCanvas.addEventListener("click", () => onClick(raycaster));
+    // Show what object is being hovered
+    canvas.addEventListener("mousemove", (evento) => onHover(evento, mouse, raycaster));
+
+    // Selects the hovered object
+    canvas.addEventListener("click", () => onClick(raycaster));
 }
 
-//------------------------------------------------ RENDERIZAR O CENARIO ---------------------------------------------------
+//------------------------------------------------ RENDERIZAR O sceneRIO ---------------------------------------------------
 function renderizar() {
     // caso exista algum objeto a ser animado no js podemos usar
     requestAnimationFrame(renderizar);
 
-    // Updating light direction when user moves camara
-    directLight.position.set(camara.position.x + 5, camara.position.y + 15, camara.position.z + 5);
-    renderer.render(cena, camara);
+    // Updating light direction when user moves camera
+    directLight.position.set(camera.position.x + 5, camera.position.y + 15, camera.position.z + 5);
+    renderer.render(scene, camera);
 }
 
-//------------------------------------------------ HOVER COM O RATO ---------------------------------------------------
+//------------------------------------------------ HOVER COM O mouse ---------------------------------------------------
 
-function onHover(evento, rato, raycaster) {
-    rato.x = (evento.clientX / myCanvas.width) * 2 - 1;
-    rato.y = -(evento.clientY / myCanvas.height) * 2 + 1;
-    raycaster.setFromCamera(rato, camara);
-
-    // vai procurar na lista de botoes se existe algum desses elementos
-    // que foram clicados
+function onHover(evento, mouse, raycaster) {
+    // Updating the mouse position
+    mouse.x = (evento.clientX / myCanvas.width) * 2 - 1;
+    mouse.y = -(evento.clientY / myCanvas.height) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
 
     var intersected = raycaster.intersectObjects(model.getParts());
 
@@ -75,10 +75,12 @@ function onHover(evento, rato, raycaster) {
 
         inter = intersected[0];
 
-        // material foi clonado, porque existem vários objetos da mesa com
-        // referencia para o mesmo material
+        // Cloning the material because exist objects sharing the same material
         inter.object.material = intersected[0].object.material.clone();
+
         inter.object.userData.oldMaterial = inter.object.material.clone();
+
+        // Giving user the prespective of something happening
         inter.object.material.color.set(0xff0000);
     } else if (inter) {
         inter.object.material = inter.object.userData.oldMaterial;
@@ -87,9 +89,6 @@ function onHover(evento, rato, raycaster) {
 }
 
 function onClick(raycaster) {
-    // vai procurar na lista de botoes se existe algum desses elementos
-    // que foram clicados
-
     var intersected = raycaster.intersectObjects(model.getParts());
 
     if (intersected.length > 0) {
@@ -100,21 +99,8 @@ function onClick(raycaster) {
 
 function change_html(object) {
     document.getElementById("obejctName").innerHTML = object.name;
+
     update_item_colors(object.userData.part.getColors());
-}
-
-function add_light_to(cena) {
-    //------------------------------------------------ ADICIONAR LUZ AMBIENTE AO CENARIO ---------------------------------------------------
-    var luzPoint = new THREE.AmbientLight(0xffffff, 4);
-    cena.add(luzPoint);
-
-    // Sun Light
-    directLight = new THREE.DirectionalLight(0xffa95c, 4);
-    directLight.castShadow = true;
-    directLight.shadow.bias = -0.0001;
-    directLight.shadow.mapSize.width = 1024 * 4;
-    directLight.shadow.mapSize.heigh = 1024 * 4;
-    cena.add(directLight);
 }
 
 function update_price(price) {
@@ -123,13 +109,13 @@ function update_price(price) {
 
 function update_item_colors(colors) {
     var html = "";
+
+    // Updating the html of the colors tab
     if (colors.length > 0) {
         for (let i = 0; i < colors.length; i++) {
             html +=
                 '<div class="col-lg-4">' +
-                '<div onclick="change_item_color(' +
-                i +
-                ')" class="item_color_card">' +
+                '<div  class="item_color_card">' +
                 '<span  class="bg-dark rounded-circle" style="height: 75px; width: 75px"></span>' +
                 "<p>" +
                 colors[i].customName +
@@ -142,13 +128,34 @@ function update_item_colors(colors) {
     }
 
     document.getElementById("item_colors").innerHTML = html;
+
+    // Find all color elements of the right side tab
+    var elements = document.getElementsByClassName("item_color_card");
+
+    // Add events that will be activated by the user interactions
+    for (let i = 0; i < elements.length; i++) {
+        // Not using addEventListener because each element just need one
+        // action for each event
+        elements[i].onclick = () => change_item_color(i, true);
+        elements[i].onmouseover = () => change_item_color(i, false);
+        elements[i].onmouseout = () => reset_item_color();
+    }
 }
 
-function change_item_color(value) {
-    var foundObject = model.findPart(clickedObject.object);
+function change_item_color(value, save) {
+    // Save temporarily the material so then it's possible to reset it
+    clickedObject.object.userData.oldMaterial = clickedObject.object.material.clone();
 
-    if (foundObject) {
-        foundObject.material.color = clickedObject.object.userData.part.colors[value];
-        foundObject.userData.oldMaterial = foundObject.material;
+    // Change the live object color
+    clickedObject.object.material.color = clickedObject.object.userData.part.colors[value];
+
+    // Change the object color permanently
+    if (save) {
+        clickedObject.object.userData.oldMaterial = clickedObject.object.material;
     }
+}
+
+function reset_item_color() {
+    // Reset the object material
+    clickedObject.object.material = clickedObject.object.userData.oldMaterial;
 }
