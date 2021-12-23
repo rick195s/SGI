561d578b8515f2b140e009c7f79325fbdb869f59
rick195s <ricardosf195@gmail.com>
2021-12-23 21:40:07 +0000

    to do change_item_texture

--- website/js/Classes/Model.js
+++ website/js/Classes/Model.js
@@ -1,6 +1,7 @@
 class Model {
     constructor() {
         this.parts = [];
+        this.textures = [];
         this.animations = [];
     }
 
@@ -13,12 +14,29 @@ class Model {
         return totalPrice;
     }
 
-    addPart(part) {
-        this.parts.push(part);
+    getTexture(name) {
+        var validTextures = [];
+        for (let index = 0; index < this.textures.length; index++) {
+            if (this.textures[index].material.localeCompare(name) == 0) {
+                validTextures.push(this.textures[index]);
+            }
+        }
+
+        return validTextures;
+    }
+
+    async addTextures(textures) {
+        console.log(await textures);
+
+        this.textures = this.textures.concat(await textures);
     }
 
     addAnimations(animations) {
-        this.animations = [...this.animations, ...animations];
+        this.animations = this.animations.concat(animations);
+    }
+
+    addPart(part) {
+        this.parts.push(part);
     }
 
     getParts() {
--- website/js/scene.js
+++ website/js/scene.js
@@ -29,14 +29,18 @@ function load_gltf_to(cena, path, model) {
                 node.castShadow = true;
                 node.receiveShadow = true;
                 node.userData.part = new Part(100);
-                node.userData.part.addColor(node.name);
-                node.userData.defaultMaterial = node.material;
+                node.userData.part.addDefaultColor(node.material.color);
+                node.userData.part.addColors(getColorsFromJson(node.name));
                 model.addPart(node);
             }
 
             update_price();
         });
 
+        gltf.parser.getDependencies("material").then((materials) => {
+            console.log(materials);
+        });
+        model.addTextures(getTexturesFromJson());
         model.addAnimations(gltf.animations);
         show_animations();
         gltf.material = new THREE.MeshStandardMaterial();
@@ -48,6 +52,44 @@ function update_window(renderer, camara, width, height) {
     renderer.setSize(width, height);
     camara.aspect = width / height;
     camara.updateProjectionMatrix();
+}
+
+async function getColorsFromJson(name) {
+    return await fetch("model_options/workBench.json")
+        .then((response) => response.json())
+        .then((data) => {
+            var colors = [];
+            if (data.parts[name]) {
+                for (var i = 0; i < data.parts[name].colors.length; i++) {
+                    var color = new Color();
+                    color.increasePrice = data.parts[name].colors[i].price;
+                    color.name = data.parts[name].colors[i].name;
+                    color.setHex(data.parts[name].colors[i].hex);
+                    colors.push(color);
+                }
+            }
+            return colors;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+async function getTexturesFromJson() {
+    return await fetch("model_options/workBench.json")
+        .then((response) => response.json())
+        .then((data) => {
+            var textures = [];
+            if (data.textures) {
+                for (var i = 0; i < data.textures.length; i++) {
+                    textures = textures.concat(data.textures);
+                }
+            }
+            return textures;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 function add_light_to(scene) {
